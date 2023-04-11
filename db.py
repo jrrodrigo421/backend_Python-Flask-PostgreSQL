919511c6f371c84df38a4dc89bf734ea4ad8665f
Rodrigo Lopes <rjr89000@gmail.com>
2023-04-11 11:34:16 -0300

    testes

--- db.py
+++ db.py
@@ -1,14 +1,21 @@
 import psycopg2
 
-db_name = "TaskListDB"
-db_user = "tasklist_user"
-db_pw = "root"
-db_host = "localhost"
+# db_name = "TaskListDB"
+# db_user = "tasklist_user"
+# db_pw = "root"
+# db_host = "localhost"
+
+
+db_name = "d4e9vv9rf1ck4t"
+db_user = "lyphsfaebehbbi"
+db_pw = "b41eab2ba62a80d0156e8e2996b2bc47bb53ac7a1dccdeee56aeeae59694a024"
+db_host = "ec2-52-54-200-216.compute-1.amazonaws.com"
+db_port = 5432
 
 
 def getTaskList():
     conn = psycopg2.connect(dbname=db_name, user=db_user,
-                            password=db_pw, host=db_host)
+                            password=db_pw, host=db_host, port=db_port)
     cur = conn.cursor()
     cur.execute('SELECT id, task_name, is_done FROM public. "TaskList"')
     tasklist = cur.fetchall()
